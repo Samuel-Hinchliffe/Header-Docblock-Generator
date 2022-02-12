ce1ff1e4dc21c271de2eaf16714becdf25cdfedd
Samuel Hinchliffe <88140540+Samuel-Hinchliffe@users.noreply.github.com>
2022-02-12 09:55:59 +0000

    DocBlock Init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,130 +1,85 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
+  <v-app id="inspire">
+    <v-system-bar app>
+      <v-spacer></v-spacer>
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <a href="https://github.com/Samuel-Hinchliffe" target="_blank">
+            <v-icon dark v-bind="attrs" v-on="on">
+              {{ svgPath }}
+            </v-icon>
+          </a>
+        </template>
+        <span>Github</span>
+      </v-tooltip>
+
+      <v-tooltip bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <a
+            href="https://www.linkedin.com/in/samuel-hinchliffe-%F0%9F%9A%80-2bb5801a5/"
+            target="_blank"
+          >
+            <v-icon dark v-bind="attrs" v-on="on">
+              {{ linkPath }}
+            </v-icon>
+          </a>
+        </template>
+        <span>Linkedin</span>
+      </v-tooltip>
+    </v-system-bar>
+
+    <v-app-bar app clipped-right flat height="72">
+      JS & PHP Docblock generator
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" app width="200">
+      <v-sheet color="grey lighten-1" height="72" width="100%">+ New Docblock</v-sheet>
+
+      <v-list shaped>
+        <v-list-item v-for="n in 5" :key="n" link>
+          <v-list-item-content>
+            <v-list-item-title>Item {{ n }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+
+    <v-footer app color="transparent" height="72" inset>
+      <div class="text-center">
+        <v-btn rounded color="primary" dark>
+          <v-icon left> {{ hammer }} </v-icon> Generate DocBlock</v-btn
         >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-          >unit-jest</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+      </div>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
+import { mdiGithub } from "@mdi/js";
+import { mdiLinkedin } from "@mdi/js";
+import { mdiHammer } from "@mdi/js";
+
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
+  data: () => ({
+    drawer: null,
+    svgPath: mdiGithub,
+    linkPath: mdiLinkedin,
+    hammer: mdiHammer,
+  }),
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+<style lang="scss">
 a {
-  color: #42b983;
+  .v-icon__svg {
+    transition: 200ms ease-in-out;
+  }
+  .v-icon__svg:hover {
+    fill: white;
+  }
 }
 </style>
